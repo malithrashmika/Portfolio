f3e82744618626f60c376fb982d60dddafba6adc
malithrashmika <malithrashmika10@gmail.com>
2024-10-15 16:34:06 +0530

    section changed

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var typed = new Typed(".text",{
-    strings: ["Full Stack Developer", "Web Developer", "UX/UI Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
+let number=document.getElementById('number');
+let count=0;
+
+setInterval(()=>{
+    if(count==65){
+        clearInterval;
+    } else {
+        count+=1;
+        number.innerHTML=`${count}%`;
+    }
+},30);
